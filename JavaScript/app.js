ee519c729e1d4bd32819131cdc55214330fa40c6
jjohannas <johanna.hornell95@gmail.com>
2018-03-12 13:46:20 +0100

    title

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -10,6 +10,9 @@ async function loadFile()
 {
     let auktionUrl = await fetchData('http://nackowskis.azurewebsites.net/api/Auktion/800/');
     let title1 = document.getElementById("title1").innerHTML = auktionUrl[0].Titel;
+    let title2 = document.getElementById("title2").innerHTML = auktionUrl[1].Titel;
+    let title3 = document.getElementById("title3").innerHTML = auktionUrl[2].Titel;
+    let title4 = document.getElementById("title4").innerHTML = auktionUrl[3].Titel;
 }
 
 loadFile();
