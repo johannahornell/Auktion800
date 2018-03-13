8ef779376c8575b389b2a28935170093550853a5
jjohannas <johanna.hornell95@gmail.com>
2018-03-13 11:09:19 +0100

    dfrtyheref

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -11,18 +11,19 @@ async function loadFile()
     let auktionUrl = await fetchData('http://nackowskis.azurewebsites.net/api/Auktion/800/');
 
 for (i = 0; i < auktionUrl.length; i++) {
-    let div = document.getElementById('auction-object' + i);
+    let auctionWrapper = document.getElementById("auction-wrapper");
+    let newDiv = document.createElement("div");
     let temp = '';
 
 
-    temp += '<br>' + '<ul>' +
+    temp += 
         '<h1> ' + auktionUrl[i].Titel + '</h1>' +
         '<h4>BESKRIVNING: ' + auktionUrl[i].Beskrivning + '</h4>' +
         '<h5>STARTDATUM: ' + auktionUrl[i].StartDatum + '</h5>' +
         '<h5>SLUTDATUM: ' + auktionUrl[i].SlutDatum + '</h5>' +
-        '<p>UTROPSPRIS: ' + auktionUrl[i].Utropspris + ' kr' + '</p>' +
-        '</ul>';
-    div.innerHTML = temp;
+        '<p>UTROPSPRIS: ' + auktionUrl[i].Utropspris + ' kr' + '</p>';
+    newDiv.innerHTML = temp;
+    auctionWrapper.appendChild(newDiv);
     }
 }
 loadFile();
