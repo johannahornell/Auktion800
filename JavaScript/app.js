191a0f8fb35ff9bb6317812155a36e7fe39e5729
jjohannas <johanna.hornell95@gmail.com>
2018-03-13 13:25:23 +0100

    right

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -12,9 +12,19 @@ async function loadFile()
 
 for (i = 0; i < auktionUrl.length; i++) {
     let auctionWrapper = document.getElementById("auction-wrapper");
-    let newDiv = document.createElement("div");
-    let temp = '';
+    let newArticle = document.createElement("article");
+    let newDivInfo = document.createElement("div");
+    let newDivBid = document.createElement("div");
 
+    let bidInput = document.createElement("input");
+    bidInput.setAttribute("type", "text");
+
+    let bidBtn = document.createElement("input");
+    bidBtn.setAttribute("type", "button");
+    bidBtn.setAttribute("value", "Bud");
+    /*bidBtn.addEventListener("click", loadBid(auktionUrl[i].AuktionID));*/
+
+    let temp = '';
 
     temp +=
         '<h1> ' + auktionUrl[i].Titel + '</h1>' +
@@ -22,8 +32,14 @@ for (i = 0; i < auktionUrl.length; i++) {
         '<h5>STARTDATUM: ' + auktionUrl[i].StartDatum + '</h5>' +
         '<h5>SLUTDATUM: ' + auktionUrl[i].SlutDatum + '</h5>' +
         '<p>UTROPSPRIS: ' + auktionUrl[i].Utropspris + ' kr' + '</p>';
-    newDiv.innerHTML = temp;
-    auctionWrapper.appendChild(newDiv);
+    newDivInfo.innerHTML = temp;
+
+    newDivBid.appendChild(bidInput);
+    newDivBid.appendChild(bidBtn);
+
+    newArticle.appendChild(newDivInfo);
+    newArticle.appendChild(newDivBid);
+    auctionWrapper.appendChild(newArticle);
 
     //gör om json datum till jämförbar tid
     endDate = new Date(auktionUrl[i].SlutDatum).getTime();
@@ -39,39 +55,6 @@ for (i = 0; i < auktionUrl.length; i++) {
         console.log('Auktionen slut');
     }
     }
-
-    for (i = 0; i < auktionUrl.length; i++) {
-        let auctionWrapper = document.getElementById("auction-wrapper");
-        let newArticle = document.createElement("article");
-        let newDivInfo = document.createElement("div");
-        let newDivBid = document.createElement("div");
-
-        let bidInput = document.createElement("input");
-        bidInput.setAttribute("type", "text");
-
-        let bidBtn = document.createElement("input");
-        bidBtn.setAttribute("type", "button");
-        bidBtn.setAttribute("value", "Bud");
-        /*bidBtn.addEventListener("click", loadBid(auktionUrl[i].AuktionID));*/
-
-        let temp = '';
-
-        temp +=
-            '<h1> ' + auktionUrl[i].Titel + '</h1>' +
-            '<h4>BESKRIVNING: ' + auktionUrl[i].Beskrivning + '</h4>' +
-            '<h5>STARTDATUM: ' + auktionUrl[i].StartDatum + '</h5>' +
-            '<h5>SLUTDATUM: ' + auktionUrl[i].SlutDatum + '</h5>' +
-            '<p>UTROPSPRIS: ' + auktionUrl[i].Utropspris + ' kr' + '</p>';
-        newDivInfo.innerHTML = temp;
-
-        newDivBid.appendChild(bidInput);
-        newDivBid.appendChild(bidBtn);
-
-        newArticle.appendChild(newDivInfo);
-        newArticle.appendChild(newDivBid);
-        auctionWrapper.appendChild(newArticle);
-
   }
-}
 
 loadFile();
